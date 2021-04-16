41d478481a6f3a8fdfb01bf76ce68653062b01b8
madison <madisonamoa@gmail.com>
2021-04-16 14:10:44 +0000

    start config index

---
+++ src/layouts/Home.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+
+
+        <h1>Hello guy</h1>
+
+    </div>
+</template>
+<script>
+    export default{
+        name: 'home'
+    }
+</script>
+
+<style>
+
+    body{
+        background-color: red;
+    }
+
+</style>
