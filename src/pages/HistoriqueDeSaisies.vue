7ac5ce0d1d3a9376022872b61d6729c1037162d0
madison <madisonamoa@gmail.com>
2021-04-22 09:20:29 +0000

    before add graphql-apollo

--- src/pages/HistoriqueDeSaisies.vue
+++ src/pages/HistoriqueDeSaisies.vue
@@ -1,12 +1,14 @@
 <template>
     <div>
 
-        <h1>Historique de saisie</h1>
 
         
 
         <div class="col-sm-12">
             <div class="card-box">
+
+        <h3>Historique de saisie</h3>
+
 
                 <!-- <select name="" id="">
                     <option value="Trie">Trier par</option>
@@ -28,38 +30,30 @@
                 <table class="table">
                     <thead class="thead-dark">
                         <tr>
-                        <th scope="col">Espece</th>
                         <th scope="col">Operation</th>
-                        <th scope="col">Periode</th>
-                        <th scope="col">Qte</th>
-                        <th scope="col">Ajouter par</th>
-                        <th scope="col">Action</th>
+                        <th scope="col">Description</th>
+                        <th scope="col">Utilisateur</th>
+                        <!-- <th scope="col">Action</th> -->
                         </tr>
                     </thead>
                     <tbody>
                         <tr>
-                        <th scope="row">Hybride</th>
-                        <td>Eclosion</td>
-                        <td>02-05-2020 -- 07-07-2020 </td>
-                        <td>255</td>
+                        <th scope="row">Eclosion</th>
+                        <td class="historique-info-add">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, nemo sapiente! Quo provident itaque corporis iste lorem ipsum </td>
                         <td>admin</td>
-                        <td><div class="myBtn bg-success">voir</div></td>
+                        <!-- <td><div class="myBtn bg-success">voir</div></td> -->
                         </tr>
                         <tr>
-                        <th scope="row">Pintade</th>
-                        <td>Eclosion</td>
-                        <td>02-05-2020 -- 07-07-2020</td>
-                        <td>387</td>
+                        <th scope="row">Eclosion</th>
+                        <td class="historique-info-delete">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, nemo sapiente! Quo provident itaque corporis iste iste lorem ipsum </td>
                         <td>admin</td>
-                        <td><div class="myBtn bg-success">voir</div></td>
+                        <!-- <td><div class="myBtn bg-success">voir</div></td> -->
                         </tr>
                         <tr>
-                        <th scope="row">Africain</th>
-                        <td>Eclosion</td>
-                        <td>02-05-2020 -- 07-07-2020</td>
-                        <td>635</td>
+                        <th scope="row">Eclosion</th>
+                        <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, nemo sapiente! Quo provident itaque corporis iste iste lorem ipsum </td>
                         <td>admin</td>
-                        <td><div class="myBtn bg-success">voir</div></td>
+                        <!-- <td><div class="myBtn bg-success">voir</div></td> -->
                         </tr>
                     </tbody>
                     </table>
@@ -85,5 +79,16 @@
         font-size: 14px;
         border-radius: 10px;
         cursor: pointer;
+    }
+
+    .historique-info-add{
+        color: green;
+    }
+
+    .historique-info-delete{
+        color: red;
+    }
+    .historique-info-update{
+        color: orange;
     }
 </style>
